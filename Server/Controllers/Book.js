f7b44c88151c61dbdd5ce6c67cf921330154ba90
“Amaal <amal.asaad26@gmail.com>
2021-03-21 13:15:06 +0200

    Book model

---
+++ Server/Controllers/Book.js
@@ -0,0 +1,89 @@
+const BookModel=require('../Models/Book');
+
+/* Add Book To DB */
+exports.addBooks= async (req,res)=>{
+    console.log(req.body);
+    const book = new BookModel({
+        name: req.body.name,
+        category: req.body.category,
+        author: req.body.author,
+        description: req.body.description,
+        bookImage: req.body.bookImage
+    })
+    console.log(book);
+    try {
+        const newBook = await book.save();
+        // const addedBook = await BookModel.findById({ _id: newBook._id }).populate('category').populate('author');
+        res.json(newBook);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+
+}
+
+/* List All Books From DB */
+exports.getAllBooks=async (req,res)=>{
+    try {
+        const books = await BookModel.find().populate('category').populate('author');
+        if(!books){
+            res.status(400);
+            return res.send({error: "books not found"});
+        }
+
+        res.json(books)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
+}
+
+/* List one Book From DB */
+exports.getOneBook=async (req,res)=>{
+    const bookId=req.params.id;
+    try{
+        const book= await BookModel.findById({_id: bookId}).populate('category').populate('author');
+        if(!book){
+            res.status(400);
+            return res.send({error: "book not found"});
+        }
+        res.json(book);
+    }catch (err){
+        return res.status(500).json(err);
+    }
+
+}
+
+/* Delete one Book From DB */
+exports.deleteBook=async (req,res)=>{
+    const bookId = req.params.id
+    try {
+        const deletedState = await BookModel.findByIdAndDelete(bookId);
+        res.json(deletedState);
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
+}
+
+/* Update one Book From DB */
+exports.editBook= async (req,res)=>{
+    const id = req.params.id;
+    const newBookData = {
+        name: req.body.name,
+        category: req.body.categoryId,
+        author: req.body.authorId,
+        bookImage: req.body.cover,
+        description:req.body.description
+    }
+    try {
+        const updatedBook = await BookModel.findByIdAndUpdate(id, newBookData, { new: true }).populate('category').populate('author');
+        if(!updatedBook){
+            res.status(400);
+            return res.send({error: "book not exist"});
+        }
+        res.json(updatedBook);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+}
