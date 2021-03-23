a3586a62e0a09f9adf1636cc8ed15006f6315f15
mahmoodfathy <mahmoodfathy26@gmail.com>
2021-03-23 13:41:04 +0200

    Feature: Added Boot Folder

--- Server/App.js
+++ Server/App.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const morgan = require('morgan');
-const categoryRoutes = require('./Routes/Category');
-const app =express();
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 
+const morgan = require("morgan");
+const categoryRoutes = require("./Routes/Category");
+const app = express();
 
 app.use(morgan("combined"));
 app.use(express.json());
@@ -13,10 +12,6 @@ app.use(cors());
 
 app.use(categoryRoutes);
 
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, ()=>{
-    console.log("mongo db is connected");
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}`);
 });
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`server is listening on port ${process.env.PORT}`);
-})
---
+++ Server/boot/dbConnection.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("mongo db is connected");
+  }
+);
