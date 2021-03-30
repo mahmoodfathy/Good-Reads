68031de832837000bfbb20395cff3394ae1ce838
Esraa350 <aesraa350@yahoo.com>
2021-03-30 09:15:18 +0200

    login3

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -10,7 +10,9 @@ import { RegisterComponent } from './components/register/register.component';
   declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],exports: [
     LoginComponent,
     RegisterComponent
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ame: string = "";
+  age:number=0;
+  myForm=new FormGroup({
+        name:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+        age:new FormControl('',[Validators.required,Validators.min(18)])
+  });
+  get nameStatus(){
+      console.log(this.myForm.controls.name.valid)
+      return this.myForm.controls.name.valid
+
+  }
+  get ageStatus(){
+      console.log(this.myForm.controls.age.valid)
+      return this.myForm.controls.age.valid
+  }
+  submitForm(){
+      console.log(this.myForm)
+      this.data.emit({name:this.myForm.value.name,age:this.myForm.value.age})
+      
+  }
+  //@Output() nameChange:EventEmitter<string>=new EventEmitter();
+  // @Output() ageChange:EventEmitter<string>=new EventEmitter();
+  // @Output() addnewrow:EventEmitter<boolean>=new EventEmitter();
+  @Output() data:EventEmitter<{name:string,age:number}>=new EventEmitter();
 
 }
